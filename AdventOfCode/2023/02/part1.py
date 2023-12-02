b49bca66fd152abd372f71b8f63862d5e387751d
AndreasFranke5 <90702500+AndreasFranke5@users.noreply.github.com>
2023-12-02 07:37:28 +0100

    Day2

---
+++ AdventOfCode/2023/02/part1.py
@@ -0,0 +1,40 @@
+import os
+
+def read(data):
+    games={}
+    for game in data.split("\n"):
+        if not game.strip():
+            continue
+        id,totalrnds=game.split(":")
+        id=int(id.split()[1])
+        rnds=totalrnds.split(";")
+        rndsamt=[]
+        for rnd in rnds:
+            colors=rnd.strip().split(",")
+            rndamt={'red': 0, 'green': 0, 'blue': 0}
+            for color in colors:
+                amt,cname=color.strip().split()
+                rndamt[cname]=int(amt)
+            rndsamt.append(rndamt)
+        games[id]=rndsamt
+    return games
+
+def possible(games, rmax, gmax, bmax):
+    pgames=[]
+    for id,rnds in games.items():
+        yes=True
+        for rndamts in rnds:
+            if rndamts['red']>rmax or rndamts['green']>gmax or rndamts['blue']>bmax:
+                yes=False
+                break
+        if yes:
+            pgames.append(id)
+    return pgames
+
+path=os.path.join(os.path.dirname(os.path.abspath(__file__)), 'input.txt')
+with open(path, "r") as file:
+    data=file.read()
+
+games=read(data)
+pgames=possible(games, 12, 13, 14)
+print(f"ID sum: {sum(pgames)}")
---
+++ AdventOfCode/2023/02/part2.py
@@ -0,0 +1,47 @@
+import os
+
+def read(data):
+    games={}
+    for game in data.split("\n"):
+        if not game.strip():
+            continue
+        id,totalrnds=game.split(":")
+        id=int(id.split()[1])
+        rnds=totalrnds.split(";")
+        rndsamt=[]
+        for rnd in rnds:
+            colors=rnd.strip().split(",")
+            rndamt={'red': 0, 'green': 0, 'blue': 0}
+            for color in colors:
+                amt,cname=color.strip().split()
+                rndamt[cname]=int(amt)
+            rndsamt.append(rndamt)
+        games[id]=rndsamt
+    return games
+
+def minimum_cubes(games):
+    min_cubes={}
+    for id,rnds in games.items():
+        minset = {'red': 0, 'green': 0, 'blue': 0}
+        for rndamt in rnds:
+            minset['red']=max(minset['red'], rndamt.get('red', 0))
+            minset['green']=max(minset['green'], rndamt.get('green', 0))
+            minset['blue']=max(minset['blue'], rndamt.get('blue', 0))
+        min_cubes[id]=minset
+    return min_cubes
+
+def powercalc(min_cubes):
+    tot=0
+    for id,cubes in min_cubes.items():
+        p=cubes['red']*cubes['green']*cubes['blue']
+        tot+=p
+    return tot
+
+path=os.path.join(os.path.dirname(os.path.abspath(__file__)), 'input.txt')
+with open(path, "r") as file:
+    data = file.read()
+
+games=read(data)
+mins=minimum_cubes(games)
+tot = powercalc(mins)
+print(f"Total power: {tot}")
