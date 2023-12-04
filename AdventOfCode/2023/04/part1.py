f16410754ddfabd52f9d148a68f0aef56f9cbaf9
AndreasFranke5 <90702500+AndreasFranke5@users.noreply.github.com>
2023-12-04 07:24:58 +0100

    day4

---
+++ AdventOfCode/2023/04/part1.py
@@ -0,0 +1,34 @@
+import os
+
+def calc_points(row):
+    left, right=row.split("|")
+    lottery_nums=left.split()
+    my_nums=right.split()
+    points=0
+    first_win=True
+
+    for num in my_nums:
+        for lottery_num in lottery_nums:
+            if num==lottery_num:
+                if first_win:
+                    points=1
+                    first_win=False
+                else:
+                    points *= 2
+                break
+    return points
+
+def read(path):
+    total=0
+    with open(path,'r') as file:
+        for line in file:
+            line=line.strip()
+            if line:
+                _, after_colon=line.split(':', 1)
+                points=calc_points(after_colon.strip())
+                total+=points
+    return total
+
+path=os.path.join(os.path.dirname(__file__),'input')
+result=read(path)
+print(f"Points: {result}")
---
+++ AdventOfCode/2023/04/part2.py
@@ -0,0 +1,31 @@
+import os
+
+def count(row):
+    left, right=row.split("|")
+    lottery_nums=left.split()
+    my_nums=right.split()
+    count=0
+    for num in my_nums:
+        for lottery_num in lottery_nums:
+            if num==lottery_num:
+                count += 1
+                break
+    return count
+
+def read(path):
+    with open(path, 'r') as file:
+        ticket=[line.strip().split(':', 1)[1].strip() for line in file if line.strip()]
+    ticket_dict={i:1 for i in range(len(ticket))}
+    i=0
+    while i<len(ticket):
+        match=count(ticket[i])
+        for j in range(1,match+1):
+            if i+j<len(ticket):
+                ticket_dict[i+j]+=ticket_dict[i]
+        i+=1
+    total=sum(ticket_dict.values())
+    return total
+
+path=os.path.join(os.path.dirname(__file__),'input')
+total = read(path)
+print(f"Total amount of tickets: {total}")
